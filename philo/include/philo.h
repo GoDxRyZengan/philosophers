70a5dca32cdcb2ac53fc743cc83cc85e2bc38a95
Hugo COULON <hucoulon@e2r2p5.clusters.42paris.fr>
2022-03-28 20:54:13 +0200

    ->maison

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -4,19 +4,12 @@
 #include <stdio.h>
 #include <pthread.h>
 
-typedef	struct	s_list
-{
-	t_list	*first;
-	t_list	*last;
-	size_t	size;
-}				t_list;
 
-
-typedef struct s_philo
+typedef struct	s_philo
 {
-	int		num_philo;
-	char	status;
-	t_list	*next;
+	int			num_philo;
+	char		status;
+	pthread_t	id;
 }				t_philo;
 
 
@@ -27,7 +20,7 @@ typedef	struct s_info
 	int		t_t_e;
 	int		t_t_s;
 	int		nb_t_e;
-	char	*status;
+	t_philo	*philo;
 }				t_info;
 
 void	init_struct(char **tab, int argc, t_info *info);
--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -1,33 +1,31 @@
 #include "../include/philo.h"
 
-void	*routine(void *arg)
+
+int		check_arg(char **argv)
 {
-	char	*status = (char *)arg;
-	status = "Thread";
-	pthread_exit(NULL);
+	int i;
+
+	i = 0;
+	while (argv[i])
+	{
+		if (check_int(argv[i]) == -1)
+			return (-1);
+		if 
+	}
 }
 
 int	main(int argc, char **argv)
 {
-//	t_info info;
-//	int	i;
-	char	*status;
-	status = "Main";
-	pthread_t philo1;
+	t_info info;
 
-//	i = 0;
-	if (argc < 5)
+	if (argc != 5 || argc != 6)
 		return (0);
-//	init_struct(argv, argc, &info);
-//	while (i < info.nb_philo)
-//	{
-//		pthread_create(&philo1, NULL, &info, NULL);
-//	}
-//	info.status = "Main";
-	printf("%s\n", status);
-	pthread_create(&philo1, NULL, &routine, &status);
-	pthread_join(philo1, NULL);
-	printf("%s\n", status);
+	if (check_arg(argv) == -1);
+		return (0);
+	info.philo = malloc(sizeof(t_philo) * (info.nb_philo + 1));
+	if (!info.philo)
+		return (0);
+	init_philo(&info);
 	return (0);
 }
 	//gerer le probleme de ft_atoi souligner par le correcteur de la derniere fois !!
